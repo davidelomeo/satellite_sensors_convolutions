38b7510454af6beb8190b376d8626a3606e2485a
Davide Lomeo <72407086+davidelomeo@users.noreply.github.com>
2023-03-06 19:06:19 +0000

    added access to srfs from package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
-reqs = ['numpy', 'pandas']
+reqs = ['numpy', 'pandas', 'pkg_resources']
 
 setup(
     name = 'convolution',
@@ -12,4 +12,9 @@ setup(
     author_email='davide.lomeo@kcl.ac.uk',
     # url='https://github.com/davidelomeo/satellite_sensors_convolutions,
     license='MIT',
+    include_package_data=True,
+    package_data={'Landsat': ['spectral_response_functions/Landsat/*.csv'],
+                  'Planet': ['spectral_response_functions/Planet/*.csv'],
+                  'Sentinel2': ['spectral_response_functions/Sentinel2/*.csv'],
+                  'Sentinel3': ['spectral_response_functions/Sentinel3/*.csv']},
 )
