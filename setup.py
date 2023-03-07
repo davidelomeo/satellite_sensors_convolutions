da13875f8c9c33156a9d0593619923896510c5af
Davide Lomeo <72407086+davidelomeo@users.noreply.github.com>
2023-03-07 10:13:05 +0000

    simplyfing directories

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import find_packages, setup
-reqs = ['numpy', 'pandas']
 
 setup(
     name = 'convolution',
+    install_requires = ['numpy', 'pandas'],
     packages = find_packages(),
-    install_requires = reqs,
     version = '0.1.0',
     description = '''Python package that to convolve input hyperspectral
                      bands to satellite sensor bands.''',
@@ -13,7 +12,7 @@ setup(
     # url='https://github.com/davidelomeo/satellite_sensors_convolutions,
     license='MIT',
     # include_package_data=True,
-    package_data={'': ['*.csv']}
+    package_data={'': ['spectral_response_functions/*.csv']}
                 #   'Planet': ['spectral_response_functions/Planet/*.csv'],
                 #   'Sentinel2': ['spectral_response_functions/Sentinel2/*.csv'],
                 #   'Sentinel3': ['spectral_response_functions/Sentinel3/*.csv']},
