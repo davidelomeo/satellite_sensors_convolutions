d66922f11e936704f13d907a98198694de7f6677
Davide Lomeo <72407086+davidelomeo@users.noreply.github.com>
2023-03-07 10:38:00 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # url='https://github.com/davidelomeo/satellite_sensors_convolutions,
     license='MIT',
     # include_package_data=True,
-    packages=find_namespace_packages(where="convolution"),
+    packages=find_namespace_packages(),
     packages_dir={'':'convolution'},
     package_data={'spectral_response_functions.Landsat': ['*.csv'],
                   'spectral_response_functions.Planet': ['*.csv'],
