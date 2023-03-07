de78c84f719c16eb6cb4eb928ec46dc03bb49b3d
Davide Lomeo <72407086+davidelomeo@users.noreply.github.com>
2023-03-07 10:47:23 +0000

    cleaned setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,5 @@ setup(
     author_email='davide.lomeo@kcl.ac.uk',
     # url='https://github.com/davidelomeo/satellite_sensors_convolutions,
     license='MIT',
-    # include_package_data=True,
     package_data={'': ['spectral_response_functions/*.csv']}
-                #   'Planet': ['spectral_response_functions/Planet/*.csv'],
-                #   'Sentinel2': ['spectral_response_functions/Sentinel2/*.csv'],
-                #   'Sentinel3': ['spectral_response_functions/Sentinel3/*.csv']},
 )
